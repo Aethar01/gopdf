405080bec224c7a3c61bb8628840e4e589cf28fa
Aethar <elliott.ashby88@gmail.com>
2026-05-10 22:53:57 +0100

    added -v version option

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	"gopdf/internal/config"
 	"gopdf/internal/viewer"
 )
 
+const version = "0.1.1"
+
 func main() {
 	var cfgPath string
 	var startPage int
+	var printVersion bool
 	flag.StringVar(&cfgPath, "config", "", "path to config.lua")
 	flag.IntVar(&startPage, "page", 1, "1-based page to open")
+	flag.BoolVar(&printVersion, "v", false, "print version")
 	flag.Parse()
+
+	if printVersion {
+		fmt.Println(version)
+		return
+	}
 
 	var docPath string
 	if flag.NArg() == 0 {
