e5b2ccad3370f91a6619416884db0e886a7529c3
Aethar <elliott.ashby88@gmail.com>
2026-05-15 00:42:11 +0100

    remove filter from file picker

--- internal/filepicker/filepicker.go
+++ internal/filepicker/filepicker.go
@@ -3,5 +3,5 @@ package filepicker
 import "github.com/sqweek/dialog"
 
 func PickPDF() (string, error) {
-	return dialog.File().Title("Open PDF").Filter("PDF files", "pdf").Load()
+	return dialog.File().Title("Open PDF").Load()
 }
