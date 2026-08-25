c8fa2530066e3d76992a53cc958490b4bb9fb825
Elliott Ashby <elliott.ashby88@gmail.com>
2026-08-25 21:13:22 +0100

    Show macOS traffic lights on titlebar hover

--- internal/viewer/native_window_darwin.m
+++ internal/viewer/native_window_darwin.m
@@ -1,5 +1,62 @@
 #import <Cocoa/Cocoa.h>
 
+static void gopdfSetTrafficLightsHidden(NSWindow *window, BOOL hidden) {
+    [[window standardWindowButton:NSWindowCloseButton] setHidden:hidden];
+    [[window standardWindowButton:NSWindowMiniaturizeButton] setHidden:hidden];
+    [[window standardWindowButton:NSWindowZoomButton] setHidden:hidden];
+}
+
+@interface GoPDFTitlebarHoverView : NSView {
+    NSTrackingArea *_trackingArea;
+}
+@end
+
+@implementation GoPDFTitlebarHoverView
+
+- (NSView *)hitTest:(NSPoint)point {
+    (void)point;
+    return nil;
+}
+
+- (void)updateTrackingAreas {
+    [super updateTrackingAreas];
+
+    if (_trackingArea != nil) {
+        [self removeTrackingArea:_trackingArea];
+        [_trackingArea release];
+        _trackingArea = nil;
+    }
+
+    _trackingArea = [[NSTrackingArea alloc]
+        initWithRect:self.bounds
+        options:(NSTrackingMouseEnteredAndExited |
+                 NSTrackingActiveAlways |
+                 NSTrackingInVisibleRect)
+        owner:self
+        userInfo:nil];
+    [self addTrackingArea:_trackingArea];
+}
+
+- (void)mouseEntered:(NSEvent *)event {
+    (void)event;
+    gopdfSetTrafficLightsHidden(self.window, NO);
+}
+
+- (void)mouseExited:(NSEvent *)event {
+    (void)event;
+    gopdfSetTrafficLightsHidden(self.window, YES);
+}
+
+- (void)dealloc {
+    if (_trackingArea != nil) {
+        [self removeTrackingArea:_trackingArea];
+        [_trackingArea release];
+    }
+    [super dealloc];
+}
+
+@end
+
 static void gopdfApplyMacOSWindowStyle(NSWindow *window) {
     if (window == nil) {
         return;
@@ -8,6 +65,27 @@ static void gopdfApplyMacOSWindowStyle(NSWindow *window) {
     window.titleVisibility = NSWindowTitleHidden;
     window.titlebarAppearsTransparent = YES;
     window.styleMask |= NSWindowStyleMaskFullSizeContentView;
+
+    NSView *contentView = window.contentView;
+    if (contentView == nil) {
+        return;
+    }
+
+    const CGFloat hoverHeight = 44.0;
+    NSRect bounds = contentView.bounds;
+    NSRect hoverFrame = NSMakeRect(
+        NSMinX(bounds),
+        NSMaxY(bounds) - hoverHeight,
+        NSWidth(bounds),
+        hoverHeight
+    );
+
+    GoPDFTitlebarHoverView *hoverView = [[GoPDFTitlebarHoverView alloc] initWithFrame:hoverFrame];
+    hoverView.autoresizingMask = NSViewWidthSizable | NSViewMinYMargin;
+    [contentView addSubview:hoverView positioned:NSWindowAbove relativeTo:nil];
+    [hoverView release];
+
+    gopdfSetTrafficLightsHidden(window, YES);
 }
 
 void gopdfConfigureMacOSWindow(void *windowPointer) {
