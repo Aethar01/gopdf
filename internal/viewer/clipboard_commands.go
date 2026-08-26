bbe0ea904800a65549e146d6b61eab203a783b42
Elliott Ashby <elliott.ashby88@gmail.com>
2026-08-26 11:12:57 +0100

    Add clipboard editing helpers for text inputs

---
+++ internal/viewer/clipboard_commands.go
@@ -0,0 +1,76 @@
+package viewer
+
+import "fmt"
+
+func (a *App) copyActiveTextInputToClipboard() bool {
+	text, active := a.activeTextInputValue()
+	if !active {
+		return false
+	}
+	if text == "" {
+		return true
+	}
+	if err := setSDLClipboardText(text); err != nil {
+		a.message = "clipboard unavailable"
+		return true
+	}
+	a.message = fmt.Sprintf("copied %d chars", len(text))
+	return true
+}
+
+func (a *App) cutActiveTextInputToClipboard() bool {
+	text, active := a.activeTextInputValue()
+	if !active {
+		return false
+	}
+	if text == "" {
+		return true
+	}
+	if err := setSDLClipboardText(text); err != nil {
+		a.message = "clipboard unavailable"
+		return true
+	}
+	switch {
+	case a.outlineMenu.visible && a.outlineMenu.searching:
+		a.updateOutlineSearchQuery("")
+	case a.luaUI.visible && a.luaUI.searching:
+		a.updateLuaUISearchQuery("")
+	case a.mode != modeNormal:
+		a.editInput(func(input *textInput) { input.Reset() })
+	}
+	a.message = fmt.Sprintf("cut %d chars", len(text))
+	return true
+}
+
+func (a *App) pasteIntoActiveTextInput() bool {
+	_, active := a.activeTextInputValue()
+	if !active {
+		return false
+	}
+	text := sdlGetClipboardText()
+	if text == "" {
+		return true
+	}
+	switch {
+	case a.outlineMenu.visible && a.outlineMenu.searching:
+		a.updateOutlineSearchQuery(a.outlineMenu.query + text)
+	case a.luaUI.visible && a.luaUI.searching:
+		a.updateLuaUISearchQuery(a.luaUI.query + text)
+	case a.mode != modeNormal:
+		a.editInput(func(input *textInput) { input.InsertText(text) })
+	}
+	return true
+}
+
+func (a *App) activeTextInputValue() (string, bool) {
+	switch {
+	case a.outlineMenu.visible && a.outlineMenu.searching:
+		return a.outlineMenu.query, true
+	case a.luaUI.visible && a.luaUI.searching:
+		return a.luaUI.query, true
+	case a.mode != modeNormal:
+		return a.input.Value, true
+	default:
+		return "", false
+	}
+}
