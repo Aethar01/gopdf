da716c7403b4a36f9e88468c5860f8f055f946a1
Elliott Ashby <elliott.ashby88@gmail.com>
2026-08-26 10:45:04 +0100

    Treat Command-C as copy shortcut

--- internal/viewer/bindings.go
+++ internal/viewer/bindings.go
@@ -46,7 +46,7 @@ func keyToken(key sdl.Keycode, mod sdl.Keymod) (string, bool) {
 	if isModifierKey(key) {
 		return "", false
 	}
-	ctrl := mod&sdl.KeymodCtrl != 0
+	ctrl := mod&sdl.KeymodCtrl != 0 || (key == sdl.KeycodeC && mod&sdl.KeymodGui != 0)
 	shift := mod&sdl.KeymodShift != 0
 	if ctrl {
 		if base, ok := baseKeyName(key); ok {
