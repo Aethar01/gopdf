4ac77f5084b2bc7ff05df654a89cd55fdee687ef
Elliott Ashby <elliott.ashby88@gmail.com>
2026-08-26 10:46:09 +0100

    Add persistent selection commands

---
+++ internal/viewer/selection_commands.go
@@ -0,0 +1,21 @@
+package viewer
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (a *App) copyPersistentSelectionToClipboard() {
+	if strings.TrimSpace(a.selection.text) == "" {
+		return
+	}
+	if err := setSDLClipboardText(a.selection.text); err != nil {
+		a.message = "clipboard unavailable"
+		return
+	}
+	a.message = fmt.Sprintf("copied %d chars", len(a.selection.text))
+}
+
+func (a *App) clearSelection() {
+	a.selection = textSelection{}
+}
